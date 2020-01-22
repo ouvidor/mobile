890725ee079d49c56809d5f86ecded9665dfb0a1
Lucas Sousa <frempyrean@gmail.com>
2020-01-22 20:49:05 -0300

    Configurações do linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'global-require': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
 };
