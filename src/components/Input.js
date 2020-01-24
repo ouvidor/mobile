68c4e0f1a56089e262a02671f59aba326f86a009
Lucas Sousa <frempyrean@gmail.com>
2020-01-23 22:05:15 -0300

    Permitindo Input dar feedback visual para erros

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,11 +3,15 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { StyleSheet } from 'react-native';
 import { fontFaces, Text } from './Text';
+import colors from '../utils/colors';
+
+const { MarioCap, BlackSirius } = colors;
 
 export const Input = styled.TextInput`
   font-size: ${props => props.fontSize || '16px'};
   font-family: ${props => fontFaces[props.fontFamily] || fontFaces.Regular};
-  border: ${StyleSheet.hairlineWidth}px black;
+  border: ${StyleSheet.hairlineWidth}px;
+  border-color: ${props => (props.erro ? MarioCap : BlackSirius)};
   border-radius: 5px;
   padding: 10px;
 `;
@@ -20,7 +24,7 @@ const Label = styled(Text)`
   padding-left: 3px;
   font-size: 16px;
   font-family: ${props => fontFaces[props.fontFamily] || fontFaces.SemiBold};
-  color: #646464;
+  color: ${props => (props.erro ? MarioCap : BlackSirius)};
 `;
 
 export const LabeledInput = ({ labelProps, inputProps }) => (
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -3,4 +3,5 @@
  */
 export default {
   MarioCap: '#FF0120', // Vermelho utilizado em erros, tirado diretamente do chapéu do Mário.
+  BlackSirius: '#646464', // Obscuro, mas nem tanto assim, tipo o Sirius.
 };
