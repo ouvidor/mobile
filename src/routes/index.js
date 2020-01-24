227d2b06302b22e087d2d5a96365a6442de3a01b
Lucas Sousa <frempyrean@gmail.com>
2020-01-23 22:05:59 -0300

    Adicionando transição na navegação

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,17 @@
 
 import React from 'react';
 import { NavigationNativeContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  TransitionPresets,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
 
 /** Importação das telas */
 import Splash from '../screens/Splash';
 import Login from '../screens/Login';
 import Home from '../screens/Home';
+import Cadastro from '../screens/Cadastro';
 
 /** Criando Stack. Navigators que sejam uma stack utilizarão Stack.Navigator e Stack.Screen */
 const Stack = createStackNavigator();
@@ -17,10 +22,18 @@ const Stack = createStackNavigator();
 export default function Routes() {
   return (
     <NavigationNativeContainer>
-      <Stack.Navigator initialRouteName="Splash" headerMode="none">
+      <Stack.Navigator
+        initialRouteName="Cadastro"
+        headerMode="none"
+        screenOptions={{
+          gestureEnabled: false,
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
       </Stack.Navigator>
     </NavigationNativeContainer>
   );
