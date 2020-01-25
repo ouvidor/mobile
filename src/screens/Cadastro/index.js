62c86137e9ce4f64d15706044d52ee7f6181734a
Lucas Sousa <frempyrean@gmail.com>
2020-01-24 22:26:39 -0300

    Iniciando cadastro

--- src/screens/Cadastro/index.js
+++ src/screens/Cadastro/index.js
@@ -1,13 +1,104 @@
-import React, { useState } from 'react';
-import { ScrollableContainer, LabeledInput } from '../../components';
-import { validarEmail } from '../../helpers';
+/* eslint-disable react/prop-types */
+import React, { useState, useEffect } from 'react';
+import {
+  ScrollableContainer,
+  LabeledInput,
+  Button,
+  Text,
+} from '../../components';
+import { validarEmail, Logar } from '../../helpers';
+import { getPostRequest } from '../../services/Api';
 
-export default function Cadastro() {
+export default function Cadastro({ navigation }) {
   const [nome, setNome] = useState();
   const [sobrenome, setSobrenome] = useState();
   const [email, setEmail] = useState();
   const [senha, setSenha] = useState();
   const [erro, setErro] = useState({});
+  const [actionError, setActionError] = useState();
+
+  useEffect(() => setActionError(null), [nome, sobrenome, email, senha]);
+
+  /**
+   *
+   * @param {string} value
+   * @param {string} field
+   * @param {function} validator
+   * @param {*string} errorMessage
+   * @author Lucas Sousa
+   * @since 2020.01.24
+   * @description
+   * Valida value utilizando validator.
+   * Se value não passar na validação, atualizo erro[field] com errorMessage
+   */
+  function validateOnBlur(value, field, validator, errorMessage) {
+    const valid = validator(value);
+    if (!valid) {
+      setErro({ ...erro, [field]: errorMessage });
+    }
+  }
+
+  /**
+   *
+   * @param {string} field
+   * @author Lucas Sousa
+   * @since 2020.01.24
+   * @description
+   * Limpa o erro de input ao ser focado.
+   */
+  function clearOnFocus(field) {
+    setErro({ ...erro, [field]: null });
+  }
+
+  /**
+   * @author Lucas Sousa
+   * @since 2020.01.24
+   * @description
+   * Valida os dados do formulário e cadastra o usuário
+   */
+  async function signUp() {
+    const requiredData = {
+      nome: { field: 'first_name', value: nome },
+      sobrenome: { field: 'last_name', value: sobrenome },
+      email: { field: 'email', value: email, validator: validarEmail },
+      senha: { field: 'password', value: senha },
+    };
+
+    let valid = true;
+    const payload = {};
+
+    /** Verificando que temos todos os dados */
+    Object.entries(requiredData).map(k => {
+      if (valid) {
+        const key = k[0];
+        const values = k[1];
+
+        if (!values.value) {
+          valid = false;
+          setErro({ ...erro, [key]: values.errorMessage || 'Campo inválido' });
+        } else if ('validator' in values) {
+          valid = values.validator(values.value);
+        }
+        if (valid) {
+          payload[values.field] = values.value;
+        }
+      }
+    });
+
+    /** Se temos todos os campos válidos, posso cadastrar o usuário */
+    if (valid) {
+      const cadastro = await getPostRequest('/user', payload);
+      console.log(payload, typeof payload.password);
+
+      if (cadastro.data.error) {
+        setActionError(cadastro.data.error);
+      } else {
+        const login = await Logar(payload.email, payload.senha);
+        console.log(login);
+        navigation.replace('Home');
+      }
+    }
+  }
 
   return (
     <ScrollableContainer>
@@ -16,6 +107,8 @@ export default function Cadastro() {
         inputProps={{
           value: nome,
           onChangeText: setNome,
+          onFocus: () => clearOnFocus('nome'),
+          errorMessage: erro.nome,
         }}
       />
       <LabeledInput
@@ -23,6 +116,8 @@ export default function Cadastro() {
         inputProps={{
           value: sobrenome,
           onChangeText: setSobrenome,
+          onFocus: () => clearOnFocus('sobrenome'),
+          errorMessage: erro.sobrenome,
         }}
       />
       <LabeledInput
@@ -30,14 +125,10 @@ export default function Cadastro() {
         inputProps={{
           value: email,
           onChangeText: setEmail,
-          onBlur: () => {
-            const emailValido = validarEmail(email);
-            if (!emailValido) {
-              setErro({ ...erro, email: true });
-            }
-          },
-          onFocus: () => setErro({ ...erro, email: false }),
-          erro: erro.email,
+          onBlur: () =>
+            validateOnBlur(email, 'email', validarEmail, 'E-mail inválido'),
+          onFocus: () => clearOnFocus('email'),
+          errorMessage: erro.email,
         }}
       />
       <LabeledInput
@@ -45,7 +136,17 @@ export default function Cadastro() {
         inputProps={{
           value: senha,
           onChangeText: setSenha,
+          secureTextEntry: true,
+          onFocus: () => clearOnFocus('senha'),
+          errorMessage: erro.senha,
         }}
+      />
+
+      <Text>{actionError}</Text>
+
+      <Button
+        touchableProps={{ onPress: signUp }}
+        textProps={{ title: 'Cadastrar' }}
       />
     </ScrollableContainer>
   );
