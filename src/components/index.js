574486c77dab2079706234457dc4ee3f04c96cc8
Lucas Sousa <frempyrean@gmail.com>
2020-01-24 22:25:56 -0300

    Exportando botão no index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { Button } from './Button';
 export { Container, CenteredContainer, ScrollableContainer } from './Container';
 export { Text } from './Text';
 export { Input, LabeledInput } from './Input';
