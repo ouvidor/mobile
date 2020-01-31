3a0ad33685d03999930f53e2929a3d71b466ac30
Lucas Sousa <frempyrean@gmail.com>
2020-01-31 20:54:43 -0300

    Adicionando mais cores

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -2,6 +2,8 @@
  * Mapeamento de cores utilizadas pelo aplicativo.
  */
 export default {
+  ErrorBackground: '#FAC0B4', // Vermelho utilizado em backgrounds de inputs com erro.
+  ErrorTextColor: '#FC5938', // Vermelho utilizado em mensagens de erro mais claras
   MarioCap: '#FF0120', // Vermelho utilizado em erros, tirado diretamente do chapéu do Mário.
   BlackSirius: '#646464', // Obscuro, mas nem tanto assim, tipo o Sirius.
   Purple: '#9762F5', // Só roxo, sem referência.
