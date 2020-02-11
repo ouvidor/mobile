5cd41dad9b41d645051fd80bb4853aabc46e1217
Lucas Sousa <frempyrean@gmail.com>
2020-02-11 19:40:33 -0300

    Iniciando tratamento de icones para as tabs e adicionando home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /**
  * Estrutura de navegação do App.
  */
@@ -17,20 +18,31 @@ import Login from '../screens/Login';
 import Home from '../screens/Home';
 import Cadastro from '../screens/Cadastro';
 import AddManifestation from '../screens/Manifestation/AddManifestation';
+import Menu from '../screens/Menu';
 
 /** Criando Stack. Navigators que sejam uma stack utilizarão Stack.Navigator e Stack.Screen */
 const Stack = createStackNavigator();
+
+function getTabBarIcon(route) {
+  const tabBarIcons = {
+    Home: <Ionicons name="ios-home" color="blue" />,
+    default: <Ionicons name="ios-home" color="green" />,
+  };
+
+  return tabBarIcons[route.name] || tabBarIcons.default;
+}
 
 const HomeTabs = createBottomTabNavigator();
 const HomeTabNavigator = () => {
   return (
     <HomeTabs.Navigator
-      screenOptions={() => ({
-        tabBarIcon: () => <Ionicons name="ios-home" color="red" />,
+      screenOptions={({ route }) => ({
+        tabBarIcon: () => getTabBarIcon(route),
       })}
     >
       <HomeTabs.Screen name="Home" component={Home} />
       <HomeTabs.Screen name="AddManifestation" component={AddManifestation} />
+      <HomeTabs.Screen name="Menu" component={Menu} />
     </HomeTabs.Navigator>
   );
 };
