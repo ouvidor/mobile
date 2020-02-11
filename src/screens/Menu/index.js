f0394c2558aa68392d4b2e988b55714ab72a4f91
Lucas Sousa <frempyrean@gmail.com>
2020-02-11 19:40:16 -0300

    Criando tela do menu (ainda em branco)

---
+++ src/screens/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ScrollableContainer, Text } from '../../components';
+
+export default function Menu() {
+  return (
+    <ScrollableContainer>
+      <Text>Menu</Text>
+    </ScrollableContainer>
+  );
+}
