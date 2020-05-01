0860bc4974cf100ddd33c7f396cab9b4106fda87
MatheusJosef <mjmatheusmj97@gmail.com>
2020-05-01 13:04:53 -0300

    Ajuste context

--- src/store/session/index.js
+++ src/store/session/index.js
@@ -1,8 +1,10 @@
+// FIXME: quando fecho e abro o app ele retorna uma promise, quando faço login,
+// retorna valor correto
 import React, { useReducer, useEffect } from 'react';
 import sessionReducer from './reducer';
 import AsyncStorage from '@react-native-community/async-storage';
 
-import Api from "../../services/Api";
+import Api from '../../services/Api';
 
 const initialState = {
   token: null,
@@ -15,26 +17,43 @@ export const SessionContext = React.createContext({
   dispatch: () => null,
 });
 
-export default function SessionContextProvider ({children}) {
+async function getStorageSession() {
+  let value = await AsyncStorage.getItem('session');
+  return value;
+}
 
-  const [session, dispatch] = useReducer(sessionReducer, initialState, async() => {
-    const localData = await AsyncStorage.getItem('user');
-    // usa os dados guardados no AsyncStorage
-    if (localData) {
-      const parsedLocalData = JSON.parse(localData);
-      const { token } = parsedLocalData;
-      Api.setToken(token) // coloca o token salvo no axios
-      return parsedLocalData;
-    }
+export default function SessionContextProvider({ children }) {
+  const [session, dispatch] = useReducer(sessionReducer, initialState, () => {
+    const localData = getStorageSession().then(sessionStorage => {
+      // usa os dados guardados no AsyncStorage
+      if (sessionStorage) {
+        const parsedLocalData = JSON.parse(sessionStorage);
+        const { token } = parsedLocalData;
+        Api.setToken(token); // coloca o token salvo no axios
+        return parsedLocalData;
+      }
+      //caso não tenha nada no AsyncStorage
+      else {
+        return initialState;
+      }
+    });
 
-    // caso não tenha nada no AsyncStorage
-    return initialState;
+    return localData;
   });
 
   useEffect(() => {
-    async function setItem() {
-      await AsyncStorage.setItem('user', JSON.stringify(session));
-      Api.setToken(session.token)
+    function setItem() {
+      if ('profile' in session) {
+        AsyncStorage.setItem('session', JSON.stringify(session));
+        Api.setToken(session.token);
+      }
+      else {
+        session.then( s => {
+          console.log('teste', s);
+          AsyncStorage.setItem('session', JSON.stringify(s));
+          Api.setToken(s.token);
+        });
+      }
     }
 
     setItem();
@@ -44,5 +63,5 @@ export default function SessionContextProvider ({children}) {
     <SessionContext.Provider value={{ session, dispatch }}>
       {children}
     </SessionContext.Provider>
-  )
+  );
 }
