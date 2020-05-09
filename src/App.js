c34e0d053648dfc9073a42626c48e3f78dbfda50
Lucas Sousa <frempyrean@gmail.com>
2020-05-09 20:10:51 -0300

    Adicionando splash (nativamente)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 /** Importando nossas rotas */
 import Routes from './routes';
 import SessionContextProvider from './store/session';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <SessionContextProvider>
       <Routes />
     </SessionContextProvider>
-  )
+  );
 }
