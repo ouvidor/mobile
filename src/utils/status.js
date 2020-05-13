767e8b038ee904663dab8c833f0f76aa921b1d25
MatheusJosef <mjmatheusmj97@gmail.com>
2020-05-13 00:35:22 -0300

    WIP status, cor e seu ícone

---
+++ src/utils/status.js
@@ -0,0 +1,42 @@
+export default {
+  1: {
+    name: 'Arquivada',
+    color: 'gray',
+    icon: 'archive',
+  },
+  2: {
+    name: 'Cadastrada',
+    color: 'blue',
+    icon: 'check',
+  },
+  3: {
+    name: 'Prorrogada',
+    color: 'yellow',
+    icon: 'check',
+  },
+  4: {
+    name: 'Resposta intermediária',
+    color: 'orange',
+    icon: 'check',
+  },
+  5: {
+    name: 'Complementada',
+    color: 'pink',
+    icon: 'file',
+  },
+  6: {
+    name: 'Resolvida',
+    color: 'red',
+    icon: 'check',
+  },
+  7: {
+    name: 'Encaminhada',
+    color: 'yellow',
+    icon: 'shuffle',
+  },
+  8: {
+    name: 'Encaminhada',
+    color: 'yellow',
+    icon: 'shuffle',
+  },
+};
