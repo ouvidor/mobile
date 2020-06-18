9a8eb3f4be2484c6e0dadb72f83a036f83e11879
Pedro Pinho <rihor1000@gmail.com>
2020-06-18 15:56:02 -0300

    WIP download de arquivo

--- src/screens/ManifestationDetails/FilesModal.js
+++ src/screens/ManifestationDetails/FilesModal.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import RNFetchBlob from 'rn-fetch-blob';
+import { showMessage } from 'react-native-flash-message';
 
+import Api from '../../services/Api';
+import colors from '../../utils/colors';
 import { Button } from '../../components';
 import {
   ModalContainer,
@@ -7,28 +11,71 @@ import {
   ModalContainerContent,
 } from './styles';
 
-import colors from '../../utils/colors';
-
 const FilesModal = ({ files }) => {
   const { globalColors } = colors;
 
-  function openFile() {
-    console.log('abrir arquivos -->', files);
+  async function openFile(file) {
+    console.log(`requisitando --> /files/${file.id}`);
+
+    const { baseURL, headers } = Api.getConfigs();
+
+    console.log();
+
+    RNFetchBlob.config({
+      addAndroidDownloads: {
+        useDownloadManager: true, // <-- this is the only thing required
+        // Show notification when response data transmitted
+        notification: true,
+        // Optional, but recommended since android DownloadManager will fail when
+        // the url does not contains a file extension, by default the mime type will be text/plain
+        mime: 'image/jpg',
+        description: 'Arquivo baixado no app Ouvidor.',
+        title: `ouvidor_arquivo${file.extension}`,
+        // Make the file scannable  by media scanner
+        mediaScannable: true,
+      },
+    })
+      .fetch('GET', `${baseURL}/files/${file.id}`, headers)
+      .then(res =>
+        RNFetchBlob.fs.scanFile([{ path: res.path(), mime: 'image/png' }])
+      )
+      .then(response => {
+        if (response) {
+          showMessage({
+            message: 'Arquivo baixado',
+            type: 'success',
+            icon: { icon: 'success', position: 'left' },
+            duration: 2000,
+          });
+        }
+      })
+      .catch(error => {
+        console.log('error: ', error);
+        showMessage({
+          message: 'Falha ao baixar o arquivo',
+          type: 'danger',
+          icon: { icon: 'danger', position: 'left' },
+          duration: 2000,
+        });
+      });
   }
 
   return (
     <ModalContainer>
       <ModalScrollView>
         <ModalContainerContent>
-          <Button
-            touchableProps={{
-              onPress: openFile,
-              background: globalColors.primaryColor,
-            }}
-            textProps={{
-              title: 'Abrir arquvio .jpg',
-            }}
-          />
+          {files.map(file => (
+            <Button
+              key={file.name}
+              touchableProps={{
+                onPress: () => openFile(file),
+                background: globalColors.primaryColor,
+              }}
+              textProps={{
+                title: `Baixar arquvio ${file.extension}`,
+              }}
+            />
+          ))}
         </ModalContainerContent>
       </ModalScrollView>
     </ModalContainer>
