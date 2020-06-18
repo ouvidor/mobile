5b46eeeb6da50377d3eee84430a228e99c5b361f
MatheusJosef <mjmatheusmj97@gmail.com>
2020-06-18 15:29:39 -0300

    Filtro para o histórico

--- src/screens/Perfil/Historic.js
+++ src/screens/Perfil/Historic.js
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect, useContext } from 'react';
+import { Text, ActivityIndicator } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 
-import { ActivityIndicator } from 'react-native';
-import { Text } from '../../components';
 import ManifestationList from '../../components/ManifestationList';
 import { ContainerWithLoading } from '../../components/Container';
+import { ContainerFilter, ButtonFilter, TextFilter, Empty } from './styles';
 import Api from '../../services/Api';
 import { SessionContext } from '../../store/session';
 import colors from '../../utils/colors';
@@ -13,16 +13,23 @@ import colors from '../../utils/colors';
 export default function Historic({ navigation }) {
   const { session } = useContext(SessionContext);
   const [userId, setUserId] = useState(null);
-  const [manifestations, setManifestations] = useState(null);
+  const [manifestations, setManifestations] = useState([]);
+  // loading da screen
   const [loading, setLoading] = useState(true);
+  // loading da lista
+  const [loadingList, setLoadingList] = useState(false);
+  // loading quando chega ao final da lista
   const [loadingPage, setLoadingPage] = useState(false);
   const [page, setPage] = useState(1);
+  const [filterSelected, setFilterSelected] = useState(1);
 
   // carrego manifestações de acordo com paginação
-  async function loadManifestations(pageToLoad = 1) {
+  async function loadManifestations(pageToLoad = 1, status = null) {
     try {
+      const statusFilter = status ? `&status=${status}` : null;
+
       const manifestationsPage = await Api.get(
-        `/manifestation/filter/?ownerId=${userId}&page=${pageToLoad}`
+        `/manifestation/filter/?ownerId=${userId}${statusFilter}&page=${pageToLoad}`
       );
 
       if (pageToLoad > manifestationsPage.last_page) {
@@ -33,16 +40,24 @@ export default function Historic({ navigation }) {
         setPage(
           manifestationsPage.rows.length >= 1 ? pageToLoad : pageToLoad - 1
         );
-        setManifestations(
-          pageToLoad >= 2
-            ? [...manifestations, ...manifestationsPage.rows]
-            : manifestationsPage.rows
-        );
-        setLoading(false);
       }
+      setManifestations(
+        pageToLoad >= 2
+          ? [...manifestations, ...manifestationsPage.rows]
+          : manifestationsPage.rows
+      );
+      setLoading(false);
+      setLoadingList(false);
     } catch (err) {
       setLoading(false);
     }
+  }
+
+  function handleFilter(filter = 1, pageToLoad = 1, status = null) {
+    setLoadingList(true);
+    setFilterSelected(filter);
+    setPage(1);
+    loadManifestations(pageToLoad, status);
   }
 
   useEffect(() => {
@@ -63,7 +78,8 @@ export default function Historic({ navigation }) {
 
   async function handleNextPage() {
     setLoadingPage(true);
-    await loadManifestations(page + 1);
+    const filter = filterSelected === 2 ? 'encerrada' : null;
+    await loadManifestations(page + 1, filter);
     setLoadingPage(false);
   }
 
@@ -78,14 +94,35 @@ export default function Historic({ navigation }) {
         />{' '}
         Meu Histórico
       </Text>
+      <ContainerFilter>
+        <ButtonFilter
+          id={1}
+          selected={filterSelected}
+          onPress={() => handleFilter()}>
+          <TextFilter>Todas</TextFilter>
+        </ButtonFilter>
+        <ButtonFilter
+          id={2}
+          selected={filterSelected}
+          onPress={() => handleFilter(2, 1, 'encerrada')}>
+          <TextFilter>Resolvida</TextFilter>
+        </ButtonFilter>
+      </ContainerFilter>
 
-      {manifestations && (
+      {manifestations && manifestations.length < 1 && !loadingList && (
+        <Empty>Não encontrado</Empty>
+      )}
+
+      {manifestations && !loadingList ? (
         <ManifestationList
           manifestations={manifestations}
           handleNextPage={handleNextPage}
           navigation={navigation}
         />
+      ) : (
+        <ActivityIndicator size="large" color={colors.Gray} />
       )}
+
       {loadingPage && <ActivityIndicator size="small" color={colors.Gray} />}
     </ContainerWithLoading>
   );
