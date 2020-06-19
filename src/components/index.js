333aeca8d7e7a17d38bebb0f2d37f280bf658965
Pedro Pinho <rihor1000@gmail.com>
2020-06-19 15:18:40 -0300

    Removendo components não utilizados

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,6 @@ export { SelectCheckbox } from './SelectCheckbox';
 export { Button, OutlinedButton } from './Button';
 export { Text, TabBarLabel } from './Text';
 export { Input, LabeledInput } from './Input';
-export { ManifestationTipoTag } from './Tags';
-export { ManifestationDetailsModal } from './Manifestation/ManifestationDetailsModal';
-export {
-  ManifestationTitle,
-  StatusHistory,
-} from './Manifestation/ManifestationDetails';
-export { Reply } from './Manifestation/ManifestationReply';
 
 export {
   Container,
